3498f46ba0292288c5cc361a800a0203bea1b491
Angel Armendariz <angelrmndrz@gmail.com>
2025-10-12 17:41:18 -0600

    Finally done

--- tar-test.sh
+++ tar-test.sh
@@ -3,11 +3,11 @@
 
 #set -x  #uncomment for execution logging - try it
 
-TARPGM=./mytar.py
+TARPGM=../mytar.py
 
 rm -rf dst # clean up from previous runs
 mkdir dst # make destination directory for extraction of tar file
-(cd src; $TARPGM c *) | (cd dst;  tar x) # pipe, first create tar file from src, then extract in dst 
+(cd src; $TARPGM c *) | (cd dst;  $TARPGM x) # pipe, first create tar file from src, then extract in dst 
 if diff -r src dst # compare src and dst directories if identical then success else failure
 then
     echo "success" >&2		# error msg to stdout
